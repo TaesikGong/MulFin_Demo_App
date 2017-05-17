b58e79db9721c284e825bdb599f4f7b099bdc58b
TaesikGong <cathena913@gmail.com>
2017-05-18 00:07:00 +0900

    initial canvas done

--- app/src/main/java/com/example/sirin_nmsl/mulfin_demo_app/MainActivity.java
+++ app/src/main/java/com/example/sirin_nmsl/mulfin_demo_app/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sirin_nmsl.mulfin_demo_app;
 
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import com.android.graphics.CanvasView;
 public class MainActivity extends Activity {
 
     private CanvasView _canvas = null;
-    private Button _btnEraser = null;
+    private Button _btnPen, _btnEraser, _btnHighlight;
     private boolean _eraser = false;
 
     @Override
@@ -20,27 +22,50 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_draw);
 
         _canvas = (CanvasView)this.findViewById(R.id.canvas);
-        _canvas.setMode(CanvasView.Mode.ERASER);
+        _btnPen = (Button)findViewById(R.id.btn_pen);
+        _btnHighlight = (Button)findViewById(R.id.btn_highlight);
         _btnEraser = (Button)findViewById(R.id.btn_eraser);
 
+        setPen();//default pen
+
+        _btnPen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setPen();
+            }
+        });
+        _btnHighlight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setHighlighter();
+            }
+        });
         _btnEraser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             _eraser = !_eraser;
-                if(_eraser)
-                {
-                    _canvas.setMode(CanvasView.Mode.ERASER);
-                }
-                else {
-                    _canvas.setMode(CanvasView.Mode.DRAW);
-                }
+                setEraser();
             }
         });
 
 
     }
 
-
-
+    void setPen()
+    {
+        _canvas.setMode(CanvasView.Mode.DRAW);
+        _canvas.setPaintStrokeWidth(10F);
+        _canvas.setPaintStrokeColor(Color.BLACK);
+    }
+    void setHighlighter()
+    {
+        _canvas.setMode(CanvasView.Mode.DRAW);
+        _canvas.setPaintStrokeWidth(50F);
+        _canvas.setPaintStrokeColor(Color.RED);
+    }
+    void setEraser()
+    {
+        _canvas.setMode(CanvasView.Mode.ERASER);
+        _canvas.setPaintStrokeWidth(50F);
+    }
 
 }
