32c21d4dc8b6bf92865edd96e640db5783593d27
TaesikGong <cathena913@gmail.com>
2017-07-11 17:54:01 +0900

    sensor calculator created

---
+++ app/src/main/java/com/example/sirin_nmsl/mulfin_demo_app/SensorCalculator.java
@@ -0,0 +1,55 @@
+package com.example.sirin_nmsl.mulfin_demo_app;
+
+/**
+ * Created by SIRIN-NMSL on 2017-07-11.
+ */
+
+public class SensorCalculator {
+
+    double min()
+    {
+
+        return 0;
+    }
+
+    double max()
+    {
+
+        return 0;
+    }
+
+    double var()
+    {
+
+        return 0;
+    }
+    /*
+    Average, std.dev
+            Variance
+    Min/max
+    RMS, derivatives of RMS
+    l1-norm
+    Squared l2 norm (mean, min/max)
+    Kurtosis
+    Infinity norm
+    Cubic spline interpolation
+            Moments
+    Skewness
+    Frobeninus norm
+    First order numerical dervatives
+    Angle, rate of change, Pearson correlation
+    Signal-Manitude Area (SMA), Average Resultant Acceleration
+*/
+    /*
+    Average, std.dev of absolute amplitude
+Normalization
+Total absolute amplitude
+Center of mass
+Estimation of the fundamental frequency
+Energy
+Entropy
+Time between peak
+Binned distribution
+
+     */
+}
