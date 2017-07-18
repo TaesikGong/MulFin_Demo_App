db23a0ac9512ef2fcadbd56ca7a50516b35dbfe5
TaesikGong <cathena913@gmail.com>
2017-07-18 12:44:14 +0900

    SensorHolder & NN classifier deprecated.

--- app/src/main/java/com/example/sirin_nmsl/mulfin_demo_app/FingerTouchEventListener.java
+++ app/src/main/java/com/example/sirin_nmsl/mulfin_demo_app/FingerTouchEventListener.java
@@ -22,10 +22,6 @@ import java.io.InputStreamReader;
 
 import weka.classifiers.Classifier;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,7 +48,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
     CanvasView _canvas = null;
     TextView _tvFinger = null;
     TypeSetter _setter = null;
-    SensorHolder _holder = null;
 
     //Sensors
     private SensorManager mSensorManager;
@@ -65,10 +60,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
     private double AccX, AccY, AccZ;
     private double GyroX, GyroY, GyroZ;
     BufferedReader[] weightFiles;
-
-    double[][] w1, w2, w3;
-    double[][] w1T, w2T, w3T;
-    double[][] b1, b2, b3;
 
     String[] files = {"0_dense_1_W.txt", "0_dense_1_b.txt",
             "1_dense_2_W.txt", "1_dense_2_b.txt",
@@ -101,99 +92,10 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
             e.printStackTrace();
         }
 
-        //Loading weight files
-        try {
-            File folder;
-            folder = new File(Environment.getExternalStorageDirectory() + "/MulFin_Weights");
-            if (!folder.exists())
-                folder.mkdir();
-
-            weightFiles = new BufferedReader[files.length];
-
-            String dir = folder.toString() + File.separator;
-
-
-            w1 = new double[8][50];
-            w2 = new double[50][30];
-            w3 = new double[30][OUTPUT_DIM];
-
-            w1T = new double[50][8];
-            w2T = new double[30][50];
-            w3T = new double[OUTPUT_DIM][30];
-
-            b1 = new double[50][1];
-            b2 = new double[30][1];
-            b3 = new double[OUTPUT_DIM][1];
-
-            for (int i = 0; i < files.length; i++) {
-                weightFiles[i] = new BufferedReader(new InputStreamReader(new FileInputStream(new File(dir + files[i]))));
-
-                double x;
-                String line;
-                String[] numbers;
-
-                // read line by line till end of file
-                for (int l = 0; (line = weightFiles[i].readLine()) != null; l++) {
-                    // split each line based on regular expression having
-                    // "any digit followed by one or more spaces".
-                    numbers = line.split("\\s+");
-
-                    for (int val = 0; val < numbers.length; val++) {
-
-                        x = Double.valueOf(numbers[val].trim());
-
-//                        Log.i("LOOP","i:"+i+" l:"+l+" val:"+val +" x:" + x);
-                        switch (i) {
-                            case 0:
-                                w1[l][val] = x;
-                                break;
-                            case 1:
-                                b1[l][val] = x;
-                                break;
-                            case 2:
-                                w2[l][val] = x;
-                                break;
-                            case 3:
-                                b2[l][val] = x;
-                                break;
-                            case 4:
-                                w3[l][val] = x;
-                                break;
-                            case 5:
-                                b3[l][val] = x;
-                                break;
-                        }
-
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //get transpose
-        for (int i = 0; i < w1.length; i++) {
-            for (int j = 0; j < w1[0].length; j++) {
-                w1T[j][i] = w1[i][j];
-            }
-        }
-        for (int i = 0; i < w2.length; i++) {
-            for (int j = 0; j < w2[0].length; j++) {
-                w2T[j][i] = w2[i][j];
-            }
-        }
-        for (int i = 0; i < w3.length; i++) {
-            for (int j = 0; j < w3[0].length; j++) {
-                w3T[j][i] = w3[i][j];
-            }
-        }
-
         //init variables
         _canvas = canvas;
         _setter = new TypeSetter(_canvas);
         _tvFinger = tvFinger;
-        _holder = new SensorHolder();
 
 
         //add sensor manages
@@ -213,8 +115,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
 
 
                 if (logOn) {
-//                    double[] input = {TSize, TPress, AccX, AccY, AccZ, GyroX, GyroY, GyroZ};
-//                    _holder.pushRow(input);
                     String[] input = {
                             String.valueOf(System.currentTimeMillis()),
                             "-1",//no finger label
@@ -251,33 +151,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
         TPress = event.getPressure();
         TSize = event.getSize();
 
-//        if (_holder.isAvailable() && isCalcNeeded) {
-//            isCalcNeeded = false;
-//            Log.i("FTEL", "Available");
-//            switch (wekaFingerClassifier()) {
-//                case THUMB:
-//                    _setter.setPen();
-//                    break;
-//                case INDEX:
-//                    _setter.setHighlighter();
-//                    break;
-//                case MIDDLE:
-//                    if (OUTPUT_DIM == 3)
-//                        _setter.setEraser();
-//                    else
-//                        _setter.setHighlighter2();
-//                    break;
-//                case RING:
-//                    _setter.setHighlighter3();
-//                    break;
-//                case LITTLE:
-//                    _setter.setEraser();
-//                    break;
-//            }
-//            _canvas.setAvailability(true, event);
-//        }
-
-        // get masked (not specific to a pointer) action
         int maskedAction = event.getActionMasked();
 
         switch (maskedAction) {
@@ -297,7 +170,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
             case MotionEvent.ACTION_POINTER_UP:
 
                 logOn = false;
-                _holder.clear();
                 _canvas.setAvailability(false, event);
 
                 break;
@@ -417,132 +289,6 @@ public class FingerTouchEventListener implements SensorEventListener, View.OnTou
                         return LITTLE;
                 }
                 break;
-
-        }
-        return 0;
-    }
-
-
-    public int fingerNNClassifier() {
-
-
-        long startTime = System.currentTimeMillis();
-
-        double[][] X1, X2, X3;
-        double[][] Y;
-        double[][] YT;
-
-        double[][] data = _holder.getTransposedData();
-
-        X1 = new double[50][_holder.getData().size()];
-        X2 = new double[30][_holder.getData().size()];
-        X3 = new double[OUTPUT_DIM][_holder.getData().size()];
-        Y = new double[OUTPUT_DIM][_holder.getData().size()];
-        YT = new double[_holder.getData().size()][OUTPUT_DIM];
-
-        //get X1, second bottleneck
-        for (int i = 0; i < w1T.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                for (int k = 0; k < w1T[0].length; k++) {
-                    X1[i][j] += w1T[i][k] * data[k][j];
-                }
-                X1[i][j] += b1[i][0];
-            }
-        }
-
-        //get X2, first bottleneck
-        for (int i = 0; i < w2T.length; i++) {
-            for (int j = 0; j < X1[0].length; j++) {
-                for (int k = 0; k < w2T[0].length; k++) {
-                    X2[i][j] += w2T[i][k] * X1[k][j];
-                }
-                X2[i][j] += b2[i][0];
-            }
-        }
-
-
-        //get X3
-        for (int i = 0; i < w3T.length; i++) {
-            for (int j = 0; j < X2[0].length; j++) {
-                for (int k = 0; k < w3T[0].length; k++) {
-                    X3[i][j] += w3T[i][k] * X2[k][j];
-                }
-                X3[i][j] += b3[i][0];
-            }
-        }
-
-
-        //softmax
-        //https://stackoverflow.com/questions/16441769/javas-bigdecimal-powerbigdecimal-exponent-is-there-a-java-library-that-does
-        for (int j = 0; j < X3[0].length; j++) {
-            double sum = 0;
-            for (int i = 0; i < X3.length; i++) {
-                sum += Math.exp(X3[i][j]);
-            }
-
-            for (int i = 0; i < X3.length; i++) {
-                Y[i][j] = Math.exp(X3[i][j]) / sum;
-            }
-        }
-
-
-        for (int i = 0; i < Y.length; i++) {
-            for (int j = 0; j < Y[0].length; j++) {
-                YT[j][i] = Y[i][j];
-            }
-        }
-
-
-        //YT = # x 5 matrix
-
-        int[] vote = {0, 0, 0, 0, 0};
-
-        for (int i = 0; i < YT.length; i++) {
-            double maxSoft = 0;
-            int maxIdx = -1;
-            for (int j = 0; j < YT[0].length; j++) {
-                if (maxSoft < YT[i][j]) {
-                    maxIdx = j;
-                    maxSoft = YT[i][j];
-                }
-            }
-            if (maxSoft >= SOFTMAX_THRESH) {
-                vote[maxIdx] += 1;
-            }
-        }
-
-
-        int seed = -1;
-        int max = 0;
-        for (int i = 0; i < OUTPUT_DIM; i++) {
-            if (vote[i] > max) {
-                max = vote[i];
-                seed = i;
-            }
-        }
-
-        Log.i("FT", "seed:" + seed + ", vote:" + vote[0] + " " + vote[1] + " " + vote[2] + " " + vote[3] + " " + vote[4]);
-        Log.i("FT", "Processing time: " + (System.currentTimeMillis() - startTime));
-
-        switch (seed) {
-            case 0:
-                _tvFinger.setText("1:Thumb");
-                return THUMB;
-            case 1:
-                _tvFinger.setText("2:Index");
-                return INDEX;
-            case 2:
-                if (OUTPUT_DIM == 3)
-                    _tvFinger.setText("5:Little");
-                else
-                    _tvFinger.setText("3:Middle");
-                return MIDDLE;
-            case 3:
-                _tvFinger.setText("4:Ring");
-                return RING;
-            case 4:
-                _tvFinger.setText("5:Little");
-                return LITTLE;
 
         }
         return 0;
